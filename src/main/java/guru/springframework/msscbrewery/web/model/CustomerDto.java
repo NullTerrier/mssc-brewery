67c7aa6d139ad0e35f25de56b9fd3261433515e9
NullTerrier <marek.aureliusz.makuch@gmail.com>
2021-09-17 18:08:29 +0200

    Added mapstruct mapping for Customer

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -1,7 +1,9 @@
 package guru.springframework.msscbrewery.web.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
@@ -10,6 +12,8 @@ import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDto {
 
     @Null
